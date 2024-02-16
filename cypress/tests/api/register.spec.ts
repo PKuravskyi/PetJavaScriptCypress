3e99ae672a78819c424418e5484c41ac3d29ea22
Pavlo Kuravskyi <pkuravsky@gmail.com>
2024-02-16 22:52:26 +0200

    update test

--- cypress/tests/api/register.spec.ts
+++ cypress/tests/api/register.spec.ts
@@ -3,7 +3,7 @@
 import { registrationEndpoint } from '../../services/endpoints/RegistrationEndpoint';
 
 describe('API: Registration', () => {
-	it.only('should register a new user', () => {
+	it('should register a new user', () => {
 		const randomUsername = new Date().getTime().toString();
 		const userData = {
 			username: randomUsername,
