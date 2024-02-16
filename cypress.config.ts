153bf5a4759ae9d35df266c2fa970ab7e565823e
Pavlo Kuravskyi <pkuravsky@gmail.com>
2024-02-16 21:43:10 +0200

    update gitignore, cypress configs

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,22 @@
 import { defineConfig } from 'cypress';
+import { configureAllureAdapterPlugins } from '@mmisty/cypress-allure-adapter/plugins';
 
 export default defineConfig({
 	projectId: 'fk32jg',
 	e2e: {
 		baseUrl: 'http://localhost:2221/',
 		specPattern: 'cypress/tests/**/*.spec.ts',
+		supportFile: 'cypress/support/e2e.ts',
+		viewportHeight: 1000,
+		viewportWidth: 1280,
 		setupNodeEvents(on, config) {
-			// implement node event listeners here
+			configureAllureAdapterPlugins(on, config);
+			return config;
 		},
+	},
+	env: {
+		allure: true,
+		allureCleanResults: true,
+		allureAttachRequests: true,
 	},
 });
